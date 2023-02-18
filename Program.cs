6638d011a02c344b92c6f0ba6b134ebd08e86330
jamesw98 <xeekatar@gmail.com>
2023-02-18 13:14:15 -0500

    can now download a json version of the spellbook

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using dnd_utils;
 using dnd_utils.Services;
 using MudBlazor.Services;
+using BlazorDownloadFile;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazorDownloadFile();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<MonsterService>();

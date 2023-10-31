92494d8ed746f7d7091b8f5e68c5645e34264f12
jamesw98 <xeekatar@gmail.com>
2023-10-31 18:22:12 -0400

    diec roller progress

--- Models/RollDetails.cs
+++ Models/RollDetails.cs
@@ -8,11 +8,45 @@ public class RollDetails
     public List<int>? Rolls { get; set; } = new();
     public int Total { get; set; }
     public bool Fixed { get; set; }
+    public bool ModOnEvery { get; set; } = false;
 
+    public string ToSimpleString()
+    {
+        var result = "";
+        if (Rolls == null)
+        {
+            return result;
+        }
+
+        var sign = Mod < 0
+            ? "-"
+            : "+";
+        
+        var modString = $"{sign} {Math.Abs(Mod ?? 0)} ";
+        
+        foreach (var r in Rolls)
+        {
+            result += $"[{r}] ";
+            if (ModOnEvery)
+            {
+                result += modString;
+            }
+        }
+
+        if (!ModOnEvery)
+        {
+            result += modString;
+        }
+        
+        return result;
+    }
+    
     public override string ToString()
     {
         if (Fixed)
+        {
             return $"Fixed roll, total: {Total}";
+        }
         
         var rolls = Rolls.Aggregate("", (current, i) => current + $"[{i}] + ");
         return $"{Num}d{Die} + {Mod} -> {rolls}{Mod} = {Total}";
--- Services/DiceService.cs
+++ Services/DiceService.cs
@@ -5,19 +5,20 @@ namespace dnd_utils.Services;
 
 public class DiceService
 {
-    private Random _rand = new();
+    private readonly Random _rand = new();
 
     public (RollDetails, RollDetails) RollDetailsNonStandard(int num, int type, int mod, AdvantageState state, bool modOnEvery=false)
     {
         if (type != 20)
+        {
             throw new Exception("(Dis)Advantage can only be used with d20s");
+        }
 
         List<RollDetails> rolls = new (new[] { RollDetailed(num, type, mod), RollDetailed(num, type, mod) });
 
-        if (state == AdvantageState.Advantage)
-            rolls = rolls.OrderByDescending(x => x.Total).ToList();
-        else 
-            rolls = rolls.OrderBy(x => x.Total).ToList();    
+        rolls = state == AdvantageState.Advantage 
+            ? rolls.OrderByDescending(x => x.Total).ToList() 
+            : rolls.OrderBy(x => x.Total).ToList();    
         
         return (rolls[0], rolls[1]);
     }
@@ -33,7 +34,9 @@ public class DiceService
             rolls.Add(currentRoll);
         }
 
-        total += modOnEvery ? mod * num : mod;
+        total += modOnEvery 
+            ? mod * num 
+            : mod;
 
         return new RollDetails
         {
@@ -42,7 +45,8 @@ public class DiceService
             Die = type,
             Mod = mod,
             Total = total,
-            Fixed = false
+            Fixed = false,
+            ModOnEvery = modOnEvery
         };
     }
     
@@ -51,9 +55,13 @@ public class DiceService
         var result = 0;
 
         for (var i = 0; i < num; i++)
+        {
             result += _rand.Next(1, type + 1);
+        }
 
-        result += modOnEvery ? mod * num : mod;
+        result += modOnEvery 
+            ? mod * num 
+            : mod;
         return result;
     }
 }

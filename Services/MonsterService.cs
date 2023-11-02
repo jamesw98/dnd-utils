0cb023b5ba41c85b0fb42ce3df7ffec83ed909b9
jamesw98 <xeekatar@gmail.com>
2023-11-01 20:41:51 -0400

    cleaned up monster service

--- Services/MonsterService.cs
+++ Services/MonsterService.cs
@@ -4,19 +4,35 @@ namespace dnd_utils.Services;
 
 public class MonsterService
 {
-    private DiceService _dice;
+    private readonly DiceService _dice;
 
     public MonsterService(DiceService dice)
     {
         _dice = dice;
     }
     
+    /// <summary>
+    /// Generates monsters based on params
+    ///
+    /// TODO - this is a lot of params, should probably just make a class, but that's a task for another day
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="number"></param>
+    /// <param name="passive"></param>
+    /// <param name="initMod"></param>
+    /// <param name="ac"></param>
+    /// <param name="hitDieType"></param>
+    /// <param name="hitDieNum"></param>
+    /// <param name="hitDieMod"></param>
+    /// <param name="fixedHp"></param>
+    /// <param name="fixedInit"></param>
+    /// <returns></returns>
     public List<Monster> GenerateMonsters(string name, int number, int passive, int initMod, int ac, int hitDieType, 
         int hitDieNum, int hitDieMod, int fixedHp=-1, int fixedInit=-1)
     {
         List<Monster> result = new();
 
-        for (int i = 0; i < number; i++)
+        for (var i = 0; i < number; i++)
         {
             var init = fixedInit == -1 ? _dice.Roll(1, 20, initMod) : fixedInit;
             var hp = fixedHp == -1 ? _dice.Roll(hitDieNum, hitDieType, hitDieMod) : fixedHp;
@@ -40,7 +56,9 @@ public class MonsterService
             
         var count = 1;
         foreach (var r in result)
+        {
             r.Name = $"{name} {count++}";   
+        }
 
         return result;
     }
